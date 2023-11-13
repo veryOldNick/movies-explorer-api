b9d4ac9843926614d8a7fc0199c439ede0f348c4
Nikolai Krasa <greyrat1@yandex.ru>
2023-11-13 21:38:50 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,9 @@ const allowedCors = [
   'http://localhost:3001',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
-];
+  'http://movie.nomoredomainsrocks.ru',
+  'http://api.movie.nomoredomainsrocks.ru',
+  ];
 
 const corsMid = (req, res, next) => {
   const { origin } = req.headers;
