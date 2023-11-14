743a302aaeadad381fbe298c0f5170635a27a608
Nikolai Krasa <greyrat1@yandex.ru>
2023-11-14 21:26:22 +0300

    cors change 2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,32 +1,52 @@
-const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://127.0.0.1:3000',
-  'http://127.0.0.1:3001',
-  'http://movie.nomoredomainsrocks.ru',
-  'http://api.movie.nomoredomainsrocks.ru',
-  'movie.nomoredomainsrocks.ru',
-  'api.movie.nomoredomainsrocks.ru',
-  ];
+// const allowedCors = [
+//   'http://localhost:3000',
+//   'http://localhost:3001',
+//   'http://127.0.0.1:3000',
+//   'http://127.0.0.1:3001',
+//   'http://movie.nomoredomainsrocks.ru',
+//   'http://api.movie.nomoredomainsrocks.ru',
+//   'movie.nomoredomainsrocks.ru',
+//   'api.movie.nomoredomainsrocks.ru',
+//   ];
+
+// const corsMid = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE';
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+
+//     return res.end();
+//   }
+
+//   return next();
+// };
+
+// module.exports = { corsMid };
 
 const corsMid = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", true);
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
 
     return res.end();
   }
 
   return next();
 };
-
 module.exports = { corsMid };
